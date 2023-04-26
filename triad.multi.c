ab52fe1db73c65822019927ea2d28ee1b2eca785
Emil VATAI <emil.vatai@gmail.com>
2023-04-26 19:08:36 +0900

    Expand work() function

--- triad.multi.c
+++ triad.multi.c
@@ -8,26 +8,13 @@ void handle_error (int retval);
 #define chk(X, msg) { retval = X; if (retval != PAPI_OK) { fprintf(stderr, "%s\n", msg); handle_error(retval); } }
 #define N (1lu << 28)
 
-double work() {
-  double a[N], b[N], c[N];
-  #pragma omp parallel for
-  for (int i = 0; i < N; i++) { a[i] = (i+3) % 11; b[i] = (3*i +2) % 29; c[i] = (5*i+1) % 13; }
-  double now = omp_get_wtime();
-  #pragma omp parallel for
-  for (int i = 0; i < N; i++) { a[i] = a[i] * b[i] + c[i]; }
-  printf("Time: %lf\n", omp_get_wtime() - now);
-  double sum = 0;
-  for (int i = 0; i < N; i++) { sum += a[i]; }
-  return sum;
-}
-
 int main() {
+  double a[N], b[N], c[N];
   int retval;
   int native = 0x0;
   int event_set = PAPI_NULL;
   long long values = 0;
   char event_str[PAPI_MAX_STR_LEN] = "FP_SCALE_OPS_SPEC";
-  double result;
 
   retval = PAPI_library_init(PAPI_VER_CURRENT);
   if (retval != PAPI_VER_CURRENT) { fprintf(stderr, "PAPI library init error!\n"); exit(1); }
@@ -37,10 +24,19 @@ int main() {
   chk(PAPI_add_event(event_set, native), "Couldn't add event.");
 
   chk(PAPI_start(event_set), "Coulnd't start event set.");
-  result = work();
+
+  #pragma omp parallel for
+  for (int i = 0; i < N; i++) { a[i] = (i+3) % 11; b[i] = (3*i +2) % 29; c[i] = (5*i+1) % 13; }
+  double now = omp_get_wtime();
+  #pragma omp parallel for
+  for (int i = 0; i < N; i++) { a[i] = a[i] * b[i] + c[i]; }
+  printf("Time: %lf\n", omp_get_wtime() - now);
+  double sum = 0;
+  for (int i = 0; i < N; i++) { sum += a[i]; }
+
   chk(PAPI_stop(event_set, &values), "Couldn't stop event set.");
   printf("counter: %lld\n", values);
-  printf("result: %f\n", result);
+  printf("result: %f\n", sum);
 
   printf("DONE!\n");
   return 0;
