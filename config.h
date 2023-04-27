3b5f7c8d22fa35d1ab2532518ddf2efbeeed1c92
Emil VATAI <emil.vatai@gmail.com>
2023-04-27 15:21:09 +0900

    Use FP_SCALE_OPS_SPEC

--- config.h
+++ config.h
@@ -2,5 +2,5 @@
 
 #define N (1lu << 28)
 
-// char event_str[PAPI_MAX_STR_LEN] = "FP_SCALE_OPS_SPEC";
-char event_str[PAPI_MAX_STR_LEN] = "PAPI_TOT_CYC";
+char event_str[PAPI_MAX_STR_LEN] = "FP_SCALE_OPS_SPEC";
+// char event_str[PAPI_MAX_STR_LEN] = "PAPI_TOT_CYC";
