17c2c72df7ad5beca723ee4da0290aac94cd5b4f
Emil VATAI <emil.vatai@gmail.com>
2023-05-01 17:47:14 +0900

    Fix array def

--- config.h
+++ config.h
@@ -3,6 +3,6 @@
 #define N 500000
 // (1lu << 18)
 
-char event_str[PAPI_MAX_STR_LEN][] = {"FP_SCALE_OPS_SPEC"};
+char event_str[][PAPI_MAX_STR_LEN] = {"FP_SCALE_OPS_SPEC"};
 // char event_str[PAPI_MAX_STR_LEN] = "PAPI_TOT_CYC";
 #define NUM_EVENTS (sizeof(event_str) / sizeof(*event_str))
