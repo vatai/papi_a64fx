760ea1e5070a08dae290d52e3ae504ce110f575f
Emil VATAI <emil.vatai@gmail.com>
2023-05-01 22:43:00 +0900

    Add one more counter

--- config.h
+++ config.h
@@ -3,6 +3,8 @@
 #define N 500000
 // (1lu << 18)
 
-char event_str[][PAPI_MAX_STR_LEN] = {"FP_SCALE_OPS_SPEC"};
-// char event_str[PAPI_MAX_STR_LEN] = "PAPI_TOT_CYC";
+char event_str[][PAPI_MAX_STR_LEN] = {
+    "FP_SCALE_OPS_SPEC",
+    "PAPI_TOT_CYC",
+};
 #define NUM_EVENTS (sizeof(event_str) / sizeof(*event_str))
