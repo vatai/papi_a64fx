2b50a23b5b5d6d2c16a43b62b54e48d1aedf25a1
Emil VATAI <emil.vatai@gmail.com>
2023-05-01 22:01:09 +0900

    Add free_values

--- triad.multi.c
+++ triad.multi.c
@@ -22,6 +22,13 @@ long long **alloc_values(size_t num_threads, size_t num_events) {
   return values;
 }
 
+void free_values(size_t tid) {
+  for (size_t tid = 0; tid < num_threads; tid++) {
+    free(values[tid]);
+    free(values);
+  }
+}
+
 int main(int argc, char *argv[]) {
   double a[N], b[N], c[N];
   int retval;
@@ -96,7 +103,7 @@ int main(int argc, char *argv[]) {
   printf("total_counter: %lld\n", total_values);
   printf("result: %f\n", sum);
 
-  free(values);
+  free_values(num_threads);
   printf("DONE!\n\n");
   return 0;
 }
