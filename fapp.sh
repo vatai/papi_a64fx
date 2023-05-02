15ce828cac1db7611204b6187f2c497bb1718204
Emil VATAI <emil.vatai@gmail.com>
2023-05-02 21:55:14 +0900

    Revert to 17 fapps

--- fapp.sh
+++ fapp.sh
@@ -3,7 +3,7 @@
 make -B
 rm -rf tmp
 mkdir tmp
-for i in $(seq 1); do
+for i in $(seq 17); do
   fapp -C -d tmp/rep${i} -Icpupa -Hevent=pa${i} ./triad.fapp
   fapp -A -d tmp/rep${i} -Icpupa -tcsv -o pa${i}.csv
 done
