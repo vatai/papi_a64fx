a5c88645de26eca11f8a6dfd25a528f8d7119c32
Emil VATAI <emil.vatai@gmail.com>
2023-05-02 21:57:33 +0900

    Better fapp dir output

--- fapp.sh
+++ fapp.sh
@@ -5,5 +5,6 @@ rm -rf tmp
 mkdir tmp
 for i in $(seq 17); do
   fapp -C -d tmp/rep${i} -Icpupa -Hevent=pa${i} ./triad.fapp
-  fapp -A -d tmp/rep${i} -Icpupa -tcsv -o pa${i}.csv
+  fapp -A -d tmp/rep${i} -Icpupa -tcsv -o tmp/pa${i}.csv
+  fapp -A -d tmp/rep${i} -Icpupa -txml -o tmp/pa${i}.xml
 done
