80448b56a301618a364afee7d1f0b529c1787547
Emil VATAI <emil.vatai@gmail.com>
2023-05-02 12:15:03 +0900

    Add all events

--- config.h
+++ config.h
@@ -4,7 +4,11 @@
 // (1lu << 18)
 
 char event_str[][PAPI_MAX_STR_LEN] = {
-    "FP_SCALE_OPS_SPEC",
-    "PAPI_TOT_CYC",
+    "FP_SCALE_OPS_SPEC",    // 0x80c0
+    "FP_FIXED_OPS_SPEC",    // 0x80c1
+    "L2D_CACHE_REFILL",     // 0x0017
+    "L2D_CACHE_WB",         // 0x0018
+    "L2D_SWAP_DM",          // 0x0325
+    "L2D_CACHE_MIBMCH_PRF", // 0x0326
 };
 #define NUM_EVENTS (sizeof(event_str) / sizeof(*event_str))
