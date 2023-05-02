b810d71852aa7b53af771fb9c60835a9d8bbb0e9
Emil VATAI <emil.vatai@gmail.com>
2023-05-02 23:05:12 +0900

    Switch array size

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #include <papi.h>
 
-// #define N 500000
-#define N (1lu << 18)
+#define N 5000000
+// #define N (1lu << 18)
 
 char event_str[][PAPI_MAX_STR_LEN] = {
     "FP_SCALE_OPS_SPEC",    // 0x80c0 0
