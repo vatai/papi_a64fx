d8fd5bc5d59554f441d74442b5a714591b977c28
Emil VATAI <emil.vatai@gmail.com>
2023-05-17 21:52:39 +0900

    Remove cntvct

--- triad.multi.c
+++ triad.multi.c
@@ -37,10 +37,8 @@ int main(int argc, char *argv[]) {
     chk(PAPI_query_event(native), "zero not an event!");
   }
   chk(PAPI_thread_init(omp_get_tid_wrapper), "PAPI_thread_init() failed.\n");
-  long long veclen = prctl(PR_SVE_GET_VL);
   double now = omp_get_wtime();
   int *event_set = malloc(num_threads * sizeof(int));
-  long long *cntvct = malloc(num_threads * sizeof(long long));
   long long **values = alloc_values(num_threads, NUM_EVENTS);
 #pragma omp parallel
   {
@@ -51,7 +49,6 @@ int main(int argc, char *argv[]) {
       chk(PAPI_add_named_event(event_set[tid], event_str[eid]),
           "Couldn't add event.");
     chk(PAPI_start(event_set[tid]), "Coulnd't start event set.");
-    cntvct[tid] = PAPI_get_real_cyc();
   }
 
 #pragma omp parallel for
@@ -62,7 +59,6 @@ int main(int argc, char *argv[]) {
 #pragma omp parallel
   {
     int tid = omp_get_tid_wrapper();
-    cntvct[tid] = PAPI_get_real_cyc() - cntvct[tid];
     chk(PAPI_stop(event_set[tid], values[tid]), "Couldn't stop event set.");
   }
   printf("Time: %lf\n", omp_get_wtime() - now);
@@ -73,19 +69,13 @@ int main(int argc, char *argv[]) {
     }
     printf("%s: %lld\n", event_str[eid], total_values);
   }
+  long long veclen = prctl(PR_SVE_GET_VL);
   for (size_t tid = 0; tid < num_threads; tid++) {
     long long *val = values[tid];
     double numer = val[0] * veclen + val[1];
-    double denum = cntvct[tid]; // cntfrq;
-    double gflops = numer / (long long)(1e9) / denum;
-    printf("cntvct: %10lld; ", cntvct[tid]);
-    for (size_t eid = 0; eid < NUM_EVENTS; eid++) {
-      printf("v[%d]: %6lld; ", eid, val[eid]);
-    }
-    printf("GFLOPS(%d): %f\n", tid, gflops);
+    double gflops = numer / (long long)(1e9);
   }
   free_values(values, num_threads);
-  free(cntvct);
   double sum = 0;
   for (int i = 0; i < N; i++) {
     sum += a[i];
